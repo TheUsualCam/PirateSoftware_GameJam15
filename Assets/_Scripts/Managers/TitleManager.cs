c285eae53c70842265d5ef35a2db200a4e1a4d4b
TheUsualCam <cameron.g.rooney@gmail.com>
2024-08-13 15:33:46 +1200

    Added tutorial section to title screen

--- Assets/_Scripts/Managers/TitleManager.cs
+++ Assets/_Scripts/Managers/TitleManager.cs
@@ -5,16 +5,73 @@ using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
-    public AudioClip uiClip;
     public Transform audioPosition;
+    public GameObject mainMenuPanel;
+    public GameObject[] tutorialPanels;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        foreach (GameObject panel in tutorialPanels)
+        {
+            panel.SetActive(false);
+        }
+
+        audioSource = GetComponent<AudioSource>();
+    }
 
     public void PlayGame()
     {
+        audioSource.Play();
         SceneManager.LoadSceneAsync(1);
+    }
+
+    public void OpenTutorialPanel()
+    {
+        audioSource.Play();
+        tutorialPanels[0].SetActive(true);
+    }
+
+    public void CloseTutorialPanels()
+    {
+        audioSource.Play();
+        foreach (GameObject panel in tutorialPanels)
+        {
+            panel.SetActive(false);
+        }
+    }
+
+    public void NextTutorialPanel()
+    {
+        audioSource.Play();
+        for (int i = 0; i < tutorialPanels.Length; i++)
+        {
+            if(tutorialPanels[i].activeInHierarchy)
+            {
+                tutorialPanels[i].SetActive(false);
+                tutorialPanels[i + 1].SetActive(true);
+                return;
+            }
+        }
+    }
+
+    public void PreviousTutorialPanel()
+    {
+        audioSource.Play();
+        for (int i = 0; i < tutorialPanels.Length;i++)
+        {
+            if(tutorialPanels[i].activeInHierarchy)
+            {
+                tutorialPanels[i].SetActive(false);
+                tutorialPanels[i - 1].SetActive(true);
+            }
+        }
     }
 
     public void ExitGame()
     {
+        audioSource.Play();
         Application.Quit();
     }
 }
